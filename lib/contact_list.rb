2ea9311e9fa10006fc9eef2e40e27463a6bf580a
김선기/결제플랫폼팀(YA) <sunki.kim@yanolja.com>
2024-04-20 01:27:48 +0900

    Add executable contact_manager file

--- lib/contact_list.rb
+++ lib/contact_list.rb
@@ -4,14 +4,6 @@ require_relative 'contact'
 
 # This class represents a contact list.
 # Each contact list has a collection of contacts.
-# Usage:
-# contact_list = ContactList.new # => #<ContactList:0x00007f8b1c8b3b20 @contacts=[]>
-# contact_list.add_contact(contact) # => [#<Contact:0x00007f8b1c8b3a98 @name="John Doe", @email="
-# contact_list.delete_contact(contact) # => []
-# contact_list.find_contact('John Doe') # => #<Contact:0x00007f8b1c8b3a98 @name="John Doe",@contacts = []>
-# contact_list.all_contact_names # => ['John Doe']
-# contact_list.search('John') # => [#<Contact:0x00007f8b1c8b3a98 @name="John Doe", @email="
-# contact_list.search('123-456-7890') # => [#<Contact:0x00007f8b1c8b3a98 @name="John Doe", @
 class ContactList
   attr_reader :contacts
 
@@ -43,14 +35,13 @@ class ContactList
 end
 
 # Example usage:
-contact_list = ContactList.new
-contact = Contact.new('John Doe', 'johndoe@example.com', '123-456-7890')
-contact_list.add_contact(contact)
-puts contact_list.find_contact('John Doe')
-puts '--------------------------'
-puts contact_list.all_contact_names
-puts '--------------------------'
-puts contact_list.search('John')
-puts '++++++++++++++++++++++++++++'
-puts contact_list.search('123-456-7890')
-# Output:  Name: John Doe Email: johndoe@example.com Phone: 123-456-7890
+# contact_list = ContactList.new
+# contact = Contact.new('John Doe', 'johndoe@example.com', '123-456-7890')
+# contact_list.add_contact(contact)
+# puts contact_list.find_contact('John Doe')
+# puts '--------------------------'
+# puts contact_list.all_contact_names
+# puts '--------------------------'
+# puts contact_list.search('John')
+# puts '++++++++++++++++++++++++++++'
+# puts contact_list.search('123-456-7890')
